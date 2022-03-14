81568203d6286efd211b635e7bfb5a886b8ec551
Danial <Danial.Dad248@gmail.com>
2022-03-14 11:36:40 -0700

    Made More folders

---
+++ Models/DTO/UserIdDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GetShitDoneBackend.Models.DTO
+{
+    public class UserIdDTO
+    {
+        
+    }
+}
