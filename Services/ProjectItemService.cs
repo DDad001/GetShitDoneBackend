c8d47495874172e6671f6dd495403f8b6932d7fb
Scotterpop73 <sctzone@aol.com>
2022-03-15 10:45:47 -0700

    Working on Project Item Service

--- Services/ProjectItemService.cs
+++ Services/ProjectItemService.cs
@@ -7,6 +7,87 @@ namespace GetShitDoneBackend.Services
 {
     public class ProjectItemService
     {
+        private readonly DataContext _context;
+        public ProjectItemService(DataContext context)
+        {
+            _context = context;
+        }
         
+        public bool AddProjectItem(ProjectItemModel newProjectItem)
+        {
+            _context.Add(newProjectItem);
+            return _context.SaveChanges() != 0;
+        }
+
+        public IEnumerable<ProjectItemModel> GetAllProjectItem()
+        {
+            return _context.ProjectItemInfo;
+        }
+
+        public ProjectItemModel GetItemsById(int id)
+        {
+            return _context.ProjectItemInfo.SingleOrDefault(item => item.Id == id);
+        }
+
+        public IEnumerable<ProjectItemModel> GetItemsByUserId(int userId)
+        {
+            return _context.ProjectItemInfo.Where(item => item.UserId == userId);
+        }
+
+
+        public ProjectItemModel GetProjectItemByTitle(string title)
+        {
+            return _context.ProjectItemInfo.Where(item => item.Title == title);
+        }
+
+        public ProjectItemModel GetProjectItemByDescription(string description)
+        {
+            return _context.ProjectItemInfo.Where(item => item.Description == description);
+        }
+
+        public ProjectItemModel GetProjectItemByDateCreated(string dateCreated)
+        {
+            return _context.ProjectItemInfo.Where(item => item.DateCreated == dateCreated);
+        }
+
+        public ProjectItemModel GetProjectItemByDueDate(string dueDate)
+        {
+            return _context.ProjectItemInfo.Where(item => item.DueDate == dueDate);
+        }
+
+        public ProjectItemModel GetProjectItemStatus(string status)
+        {
+            return _context.ProjectItemInfo.Where(item => item.Status == status);
+        }
+
+        public ProjectItemModel GetProjectItemByAMemberId(int memberId)
+        {
+            return _context.ProjectItemInfo.Where(item => item.UserId == memberId);
+        }
+
+        public ProjectItemModel GetProjectItemByAMemberUsername(string memberUsername)
+        {
+            return _data.GetProjectItemByAMemberUsername(memberUsername);
+        }
+
+        public ProjectItemModel GetProjectItemByAMemberUsername(string memberUsername)
+        {
+            return _data.GetProjectItemByAMemberUsername(memberUsername);
+        }
+
+        public IEnumerable<ProjectItemModel> GetDeletedProjectItems()
+        {
+            return _data.GetDeletedProjectItems();
+        }
+
+        public IEnumerable<ProjectItemModel> GetArchivedProjectItems()
+        {
+            return _data.GetArchivedProjectItems();
+        }
+
+        public bool UpdateProjectItem(ProjectItemModel updatedProject)
+        {
+            return _data.UpdateProjectItem(updatedProject);
+        }
     }
 }
