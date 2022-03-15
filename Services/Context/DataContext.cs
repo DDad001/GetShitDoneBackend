c8535b5c8b5b8e5ac7daede44db5698570e0be50
trentbarboza <tbarboza@codestack.co>
2022-03-15 11:11:19 -0700

    added taskitemmodel to data context

--- Services/Context/DataContext.cs
+++ Services/Context/DataContext.cs
@@ -11,6 +11,7 @@ namespace GetShitDoneBackend.Services.Context
     {
         public DbSet<UserModel> UserInfo { get; set;}
         public DbSet<ProjectItemModel> ProjectItemInfo { get; set; }
+        public DbSet<TaskItemModel> TaskItemInfo { get; set; }
         public DataContext(DbContextOptions options ): base(options)
         { }
         protected override void OnModelCreating(ModelBuilder builder)
