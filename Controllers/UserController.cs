c783763854ed1471abb98773158ebcf98426323d
dylanmcfarlin <dylanmcfarlin10@gmail.com>
2022-03-15 09:22:31 -0700

    some userController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -10,6 +10,48 @@ namespace GetShitDoneBackend.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        
+        private readonly UserService _data;
+
+        public UserController(UserService dataFromService) {
+            _data = dataFromService;
+        }
+
+        [HttpGet("UserByUsername/{username}")]
+        public UserIdDTO GetUserByUsername(string username)
+        {
+            return _data.GetUserIdDTOByUsername(username);
+        }
+
+        [HttpPost("AddUser")]
+        public bool AddUser(CreateAccountDTO UserToAdd) {
+            return _data.AddUser(UserToAdd);
+        }
+
+        [HttpGet("GetAllUsers")]
+        public IEnumerable<UserModel> GetAllUsers() 
+        {
+            return _data.GetAllUsers();
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] LoginDTO User)
+        {
+            return _data.Login(User);
+        }
+
+        //Update User Account
+
+        [HttpPost("UpdateUser/{username}")]
+        public bool UpdateUser(string username)
+        {
+            return _data.UpdateUsername(username);
+        }
+
+        //Delete User Account
+        [HttpPost("DeleteUser/{userToDelete}")]
+        public bool DeleteUser(string? userToDelete)
+        {
+            return _data.DeleteUser(userToDelete);
+        }
     }
 }
