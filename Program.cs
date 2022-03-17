2badb86cf15cf6f73df6abf8bf92ec9c88997da1
dylanmcfarlin <dylanmcfarlin10@gmail.com>
2022-03-17 10:23:55 -0700

    Added ports

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(conne
 builder.Services.AddCors(options => {
     options.AddPolicy("JiraPolicy",
     builder => {
-        builder.WithOrigins("http://localhost:3000")
+        builder.WithOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002")
             .AllowAnyHeader()
             .AllowAnyMethod();
 
